231e3528a5094023bb88b96e6b769eb2824bd13f
ICRAVC <leafar1314@gmail.com>
2021-03-25 12:34:56 -0600

    Refactory directories

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SharedModule } from '../../shared/shared.module';
 import { RoleRepository } from './role.repository';
 import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoleRepository]), SharedModule],
+  imports: [TypeOrmModule.forFeature([RoleRepository])],
   providers: [RoleService],
   controllers: [RoleController],
 })
